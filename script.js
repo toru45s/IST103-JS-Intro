d0b24313cd10e53c256ece07737dba274a2f7c98
t-yaginuma <toru45s@gmail.com>
2025-03-10 20:21:51 -0700

    Tohru - Class Activity2 - 10 March 2025 #1

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ const lastName = "Yaginuma";
 const fullName = firstName + lastName;
 const age = 100;
 const hasWorkVisa = true;
+const hasStudentVisa = true;
 
 console.log("firstName is " + firstName);
 console.log("lastName is " + lastName);
 console.log("fullName is " + fullName);
 console.log("age is " + age);
 console.log("hasWorkVisa is " + hasWorkVisa);
+console.log("hasStudentVisa is " + hasStudentVisa);s
