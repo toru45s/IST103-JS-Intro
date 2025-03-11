a257a75a511e49b838476f61536b3ed2c3d18231
t-yaginuma <toru45s@gmail.com>
2025-03-10 20:14:06 -0700

    add console.log

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const lastName = "Yaginuma";
 const fullName = firstName + lastName;
 const age = 100;
 const hasWorkVisa = true;
+
+console.log(firstName);
+console.log(lastName);
+console.log(fullName);
+console.log(age);
+console.log(hasWorkVisa);
