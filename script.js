31c9a04a6f5e8976e9e242f507894658fcc9c83a
t-yaginuma <toru45s@gmail.com>
2025-03-10 20:13:36 -0700

    update variables

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-alert("Hello World");
+// alert("Hello World");
+const firstName = "Tohru";
+const lastName = "Yaginuma";
+const fullName = firstName + lastName;
+const age = 100;
+const hasWorkVisa = true;
